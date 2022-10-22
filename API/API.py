68f2747f46ea15efd25dbb6c05c282d311fc7f70
Ahmad Haris Kurniawan <ahmadharisk@gmail.com>
2022-10-22 14:23:27 +0700

    add dashboard menu

--- API/API.py
+++ API/API.py
@@ -1,23 +1,51 @@
 import json
 import os
+import time
+
+PATH_BOOKS = "databases\\Book.json"
+PATH_USER = "databases\\User.json"
 
 # user json
-def get_user():
-    with open("databases\\User.json", "r") as json_file:
-        user = json.load(json_file)
-        return user 
+def get_user(username):
+    with open(PATH_USER, "r") as json_file:
+        users = json.load(json_file)
+        return filter(lambda user: user["username"] == username, users)
 
-def create_user():
-    is_user_json_exist = os.path.exists("databases\\User.json") 
-    if not is_user_json_exist:
+# create DB
+def create_db(db_name: str, data: list):
+    path = "databases"
+    is_folder_db_exist = os.path.exists("databases")
+    if not is_folder_db_exist:
         os.mkdir("databases")
-        admin = {"username": "admin", "password": "admin"}
-        json_string = json.dumps(admin)
-        with open("databases\\User.json", "w") as user:
-            user.write(json_string)
-            user.close()
-        print("berhasil membuat databases user")
-        input("tekan Enter untuk melanjutkan ke halaman login")
-    
-    if is_user_json_exist:
-        print("berhasil terhubung ke databases user")
+    is_db_exist = os.path.exists(f"{path}\\{db_name}")
+    if not is_db_exist:
+        json_string = json.dumps(data)
+        with open(f"{path}\\{db_name}", "w") as db:
+            db.write(json_string)
+            db.close()
+        print(f"berhasil membuat {db_name}")
+    if is_db_exist:
+        print(f"berhasil terhubung ke databases {db_name}")
+
+# book json
+def get_total_book():
+    with open(PATH_BOOKS, "r") as json_file:
+        books = json.load(json_file)
+        return len(books) 
+
+def get_all_books():
+    with open(PATH_BOOKS, "r") as json_file:
+        books = json.load(json_file)
+        return books
+
+def post_book(book_title, book_author, year_of_publication, publisher, created_by):
+    books = get_all_books()
+    book_id = len(books) + 1
+    book = {
+            "book_id": book_id,
+            "book_title": book_title,
+            "book_author": book_author,
+            "year_of_publication":year_of_publication,
+            "publisher": publisher,
+            "created_at": int(round(time.time() * 1000))
+            }    
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
-import json
 import shutil
 import getpass
 import sys
 from API import API as api
+from colors import bcolors
 
 class LibraryApp:
     """LibraryApp administration"""
@@ -11,11 +11,14 @@ class LibraryApp:
         os.system("cls")
         self.is_login = False
         self.size_terminal = shutil.get_terminal_size().columns
-        api.create_user()
+        api.create_db("User.json", [{"username": "admin", "password": "admin"}])
+        api.create_db("Book.json", [])
+        input("Tekan enter untuk melanjutkan login")
         self.header()
         self.login_menu()
 
     def header(self):
+        """header every page must have"""
         os.system("cls")
         print(".--.           .---.        .-.".center(self.size_terminal))
         print("   .---|--|   .-.     | A |  .---. |~|    .--.".center(self.size_terminal))
@@ -28,16 +31,16 @@ class LibraryApp:
         print("")
         print(" Selamat datang di perpustakaan ".center(self.size_terminal, "*"))
         print("*"*self.size_terminal)
-        print("")
 
     def login_validation(self, username, password):
-        user = api.get_user()
+        user = list(api.get_user(username))
+        user = user[0]
         result = {}
 
         if username != user["username"]:
             result["status"] = False
             result["detail"] = "username tidak sesuai"
-            return result 
+            return result
 
         if password != user["password"]:
             result["status"] = False
@@ -47,12 +50,13 @@ class LibraryApp:
         result["status"] = True
         result["detail"] = "berhasil login"
 
-        return result 
+        return result
 
     def login_menu(self):
         is_password_correct = False
         count_incorrect = 0
-        
+        username = ""
+      
         while ((not is_password_correct) and (count_incorrect <= 2)):
             username = input("Username: ")
             password = getpass.getpass("Password: ")
@@ -62,19 +66,46 @@ class LibraryApp:
             is_password_correct = result["status"]
             count_incorrect += 1
 
-        if is_password_correct: 
+        if is_password_correct:
             self.is_login = True
-            return self.dashboard_menu()
+            return self.dashboard_page(username)
 
         return sys.exit()
 
-    def dashboard_menu(self):
+    def dashboard_page(self, username):
         os.system("cls")
-        self.header()
-        print("dashboard")
+        total_books = api.get_total_book()
+        if self.is_login:
+            self.header()
+            user = list(api.get_user(username))
+            user = user[0]
+            print(f"selamat datang, {user['username']}".rjust(self.size_terminal))
+            print(f"{bcolors.HEADER}INFORMASI{bcolors.ENDC}")
+            print("-"*self.size_terminal)
+            print(f"{'Total Buku': <10}{'Total Buku Dipinjam': ^25}{'Total Buku Mendakati Jatuh Tempo': ^40}{'Total Buku Melewati Batas Pengembalian': >40}")
+            print(f"{total_books: ^10}")
+            print("="*self.size_terminal)
+            self.dashboard_menu()
+            input()
 
-
-
+    def dashboard_menu(self):
+        print("Menu Aplikasi Perpustakaan")
+        print("+"*30)
+        print("1. tambah buku")
+        print("2. update buku")
+        print("3. hapus buku")
+        print("5. lihat daftar buku")
+        print("6. peminjaman buku")
+        print("7. pengembalian buku")
+        print("8. lihat daftar buku dipinjam")
+        print("9. keluar aplikasi")
+        menu = -1
+        while menu == -1:
+            try:
+                menu = int(input("masukkan pilihan anda: "))
+            except:
+                print(f"{bcolors.FAIL}menu tidak tersedia{bcolors.ENDC}")
+        return menu
 
 if __name__ == "__main__":
     app = LibraryApp()
